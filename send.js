9b8e63f149bfcec5e1b8edf30f550cb10ce19110
Ivan Chuvakhin <Chuvakhin@rospartner.ru>
2020-03-16 17:58:42 +0300

    текст2

--- send.js
+++ send.js
@@ -2,14 +2,16 @@ var chatid = "AoLFvMEflv-oMgnfRxg";
 var token = "001.1125707302.2110502077:751305137";
 
 function otpravka(token,chatid,new_line){
+  var names = document.getElementById("name").value;
+  var phones = document.getElementById("phone").value;
   var src = document.getElementById("message").value;
   //var text = src.split(new_line || /\r?\n/);
   var text = src.split(new_line || "%0A");
   var text2 = text.join("%0A");
-  //<u>В Москве выявлено 33&nbsp;случая заболевания&nbsp;</u>
+  var text3 = "ФИО: " + names + "%0A" + "Телефон: " + phones + "%0A" + "Сообщение:%0A" + text2
   var z=$.ajax({
   type: "GET",  
-  url: "https://api.icq.net/bot/v1/messages/sendText?token="+token+"&chatId="+chatid+"&text="+text2,
+  url: "https://api.icq.net/bot/v1/messages/sendText?token="+token+"&chatId="+chatid+"&text="+text3,
   }); 
  };
  function check() {
