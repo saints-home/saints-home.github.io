58559a7a4a2e9f88a500e22f03f5efc3827f6145
Ivan Chuvakhin <Chuvakhin@rospartner.ru>
2020-03-16 16:01:35 +0300

    qwe

--- send.js
+++ send.js
@@ -1,13 +1,13 @@
-var chatid = "-1001312538029";
-var token = "1133420646:AAHHXNVYr8bVFMnr1Lnpwdc-THU1FtH87HQ";
+var chatid = "AoLFvMEflv-oMgnfRxg";
+var token = "001.1125707302.2110502077:751305137";
 var text = "Текст для <b>нашего</b> бота";
 //Отправляем текст в наш телеграм канал
 //otpravka(token,text,chatid);
  
 function otpravka(token,text,chatid){
   var z=$.ajax({
-  type: "POST",  
-  url: "https://api.telegram.org/bot"+token+"/sendMessage?chat_id="+chatid,
+  type: "GET",  
+  url: "https://api.icq.net/bot/v1/messages/sendText?token="+token+"&chatId="+chatid,
   data: "parse_mode=HTML&text="+encodeURIComponent(text),
   }); 
  };
