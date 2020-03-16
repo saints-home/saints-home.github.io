60bac1147c1e142a0e16fcde8acf3259223e43e9
Ivan Chuvakhin <Chuvakhin@rospartner.ru>
2020-03-16 23:13:41 +0300

    ыфвфывфыв

--- send.js
+++ send.js
@@ -5,14 +5,21 @@ function otpravka(token,chatid,new_line){
   var names = document.getElementById("name").value;
   var phones = document.getElementById("phone").value;
   var src = document.getElementById("message").value;
-  //var text = src.split(new_line || /\r?\n/);
   var text = src.split(new_line || "%0A");
   var text2 = text.join("%0A");
   var text3 = "ФИО: " + names + "%0A" + "Телефон: " + phones + "%0A" + "Сообщение:%0A" + text2
   var z=$.ajax({
-  type: "GET",  
+  type: "POST",  
   url: "https://api.icq.net/bot/v1/messages/sendText?token="+token+"&chatId="+chatid+"&text="+text3,
-  }); 
+  });
+  document.getElementById("name").value = "";
+  document.getElementById("phone").value = "";
+  document.getElementById("message").value = "";
+  document.getElementById("name").onkeyup=check();
+  document.getElementById("phone").onkeyup=check();
+  document.getElementById("message").onkeyup=check();
+  document.getElementById('formSend').style.display = "none";
+  document.getElementById('formSendGoal').style.display = "block";
  };
  function check() {
   var inp1 = document.getElementById('name'),
