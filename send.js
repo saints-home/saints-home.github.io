5253ca2be3b6b40381340e57074dbde434d5a84e
Ivan Chuvakhin <Chuvakhin@rospartner.ru>
2020-03-17 21:30:40 +0300

    123

--- send.js
+++ send.js
@@ -7,7 +7,9 @@ function otpravka(token,chatid,new_line){
   var src = document.getElementById("message").value;
   var text = src.split(new_line || "%0A");
   var text2 = text.join("%0A");
-  var text3 = "ФИО: " + names + "%0A" + "Телефон: " + phones + "%0A" + "Сообщение:%0A" + text2
+  var number = Math.random() * 1000000000;
+  var number2 = 'N' + number.toFixed(0);
+  var text3 = "ФИО: " + names + "%0A" + "Телефон: " + phones + "%0A" + "Сообщение:%0A" + text2 + "%0A" + "Номер: " + number2;
   var z=$.ajax({
   type: "POST",  
   url: "https://api.icq.net/bot/v1/messages/sendText?token="+token+"&chatId="+chatid+"&text="+text3,
@@ -20,6 +22,7 @@ function otpravka(token,chatid,new_line){
   document.getElementById("message").onkeyup=check();
   document.getElementById('formSend').style.display = "none";
   document.getElementById('formSendGoal').style.display = "block";
+  document.getElementById("numberZakaz").innerText = number2;
  };
  function check() {
   var inp1 = document.getElementById('name'),
