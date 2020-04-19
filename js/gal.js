c2ae1d7708a3c8cbbad7dea960cee8968d5bac3d
Ivan Chuvakhin <chuvakhin@rospartner.ru>
2020-04-19 19:26:00 +0300

    asdasdas

--- js/gal.js
+++ js/gal.js
@@ -1,30 +1,48 @@
-var mas = ["1.jpg", "2.jpg", "3.jpg", "4.jpg"] // массив картинок
-var to = -1; // Счетчик, указывающий на текущую картинки
+var toArt = 0;
+var toPhoto = 0;
 
-function right_arrow() // Открытие следующей картинки(движение вправо)
-{
-    var obj = document.getElementById("img");
-    if (to < mas.length) {
-        to++;
-        obj.src = mas[to];
-        console.log(to.toString() + " " + mas[to] + " " + obj.src);
-    }
-    if (to == mas.length - 1) {
-        to = -1;
-        console.log(to.toString() + " " + mas[to] + " " + obj.src);
+var art = ["image/art/1.jpg", "image/art/2.jpg"];
+var photo = ["image/photo/3.jpg", "image/photo/4.jpg"];
+
+
+function openArt() {
+    var photoBody = document.getElementById("photoBody");
+    photoBody.style.display = "none";
+    var artBody = document.getElementById("artBody");
+    artBody.style.display = "block";
+
+    toArt = 0;
+    var objArtBody = document.getElementById("imgArt");
+    objArtBody.src = art[toArt];
+}
+
+function nextArt() {
+    var objArtBody = document.getElementById("imgArt");
+    toArt++;
+    objArtBody.src = art[toArt];
+    if (toArt == art.length) {
+        toArt = 0;
+        objArtBody.src = art[toArt];
     }
 }
 
-function left_arrow() // Открытие предыдущей картинки(движение влево)
-{
-    var obj = document.getElementById("img");
-    if (to <= mas.length - 1) {
-        obj.src = mas[to];
-        to--;
-        console.log(to.toString() + " to-- left " + mas[to]);
-    }
-    if (to == 0) {
-        to = mas.length - 1;
-        console.log(to.toString() + " to-- left " + mas[to]);
+function openPhotu() {
+    var photoBody = document.getElementById("photoBody");
+    photoBody.style.display = "block";
+    var artBody = document.getElementById("artBody");
+    artBody.style.display = "none";
+
+    toPhoto = 0;
+    var objPhotoBody = document.getElementById("imgPhoto");
+    objPhotoBody.src = photo[toPhoto];
+}
+
+function nextPhoto() {
+    var objPhotoBody = document.getElementById("imgPhoto");
+    toPhoto++;
+    objPhotoBody.src = photo[toPhoto];
+    if (toPhoto == photo.length) {
+        toPhoto = 0;
+        objPhotoBody.src = photo[toPhoto];
     }
 }
