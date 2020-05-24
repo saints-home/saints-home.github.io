bf1b7b383238d1b501a342b5d4556e6ff9689c74
Ivan Chuvakhin <Chuvakhin@rospartner.ru>
2020-05-24 22:23:05 +0300

    sadasdas

--- js/gal.js
+++ js/gal.js
@@ -1,88 +1,69 @@
 //Функции управления Арт-иллюстрациями
-var art = ["image/art/1.jpg", "image/art/2.jpg", "https://lh4.googleusercontent.com/Q-ovXG5KJLcROK8sUqtPGXrsjbRrJN4GfZYBbRCy4w3P-jC3skjN_ojexDwGumJgj1Mg15PLOipXul04ZQwm=w1600-h795-rw"];
+var art = ["image/art/1.jpg", "image/art/2.jpg"];
 var toArt = 0;
-var artZoom = "50% 50%"
-var artZoom2 = "100%"
 
 function openArt() {
-    var photoBody = document.getElementById("photoBody");
+    var photoBody = document.getElementById("photo");
     photoBody.style.display = "none";
-    var artBody = document.getElementById("artBody");
+    var artBody = document.getElementById("art");
     artBody.style.display = "block";
 
     toArt = 0;
-    var objArtBody = document.getElementById("imgChange");
-    objArtBody.style.background = "url(" + art[toArt] + ") " + artZoom;
-    objArtBody.style.backgroundSize = artZoom2;
-    objArtBody.style.backgroundRepeat = "no-repeat"
+    var objArtBody = document.getElementById("artBody");
+    objArtBody.src = art[toArt];
 }
 
 function nextArt() {
-    var objArtBody = document.getElementById("imgChange");
+    var objArtBody = document.getElementById("artBody");
     toArt++;
-    objArtBody.style.background = "url(" + art[toArt] + ") " + artZoom;
-    objArtBody.style.backgroundSize = artZoom2;
-    objArtBody.style.backgroundRepeat = "no-repeat"
+    objArtBody.src = art[toArt];
     if (toArt == art.length) {
         toArt = 0;
-        objArtBody.style.background = "url(" + art[toArt] + ") " + artZoom;
-        objArtBody.style.backgroundSize = artZoom2;
-        objArtBody.style.backgroundRepeat = "no-repeat"
+        objArtBody.src = art[toArt];
     }
 }
 
 function prevArt() {
-    var objArtBody = document.getElementById("imgChange");
+    var objArtBody = document.getElementById("artBody");
     if (toArt == 0) {
         toArt = art.length;
-        objArtBody.style.background = "url(" + art[toArt] + ") " + artZoom;
-        objArtBody.style.backgroundSize = artZoom2;
-        objArtBody.style.backgroundRepeat = "no-repeat"
+        objArtBody.src = art[toArt];
     }
     toArt--;
-    objArtBody.style.background = "url(" + art[toArt] + ") " + artZoom;
-    objArtBody.style.backgroundSize = artZoom2;
-    objArtBody.style.backgroundRepeat = "no-repeat"
+    objArtBody.src = art[toArt];
 }
 
 //Функции управления Фотографиями
 var photo = ["image/photo/3.jpg", "image/photo/4.jpg"];
 var toPhoto = 0;
-var photoZoom = "50% 50%"
-var photoZoom2 = "115%"
 
 function openPhotu() {
-    var photoBody = document.getElementById("photoBody");
+    var photoBody = document.getElementById("photo");
     photoBody.style.display = "block";
-    var artBody = document.getElementById("artBody");
+    var artBody = document.getElementById("art");
     artBody.style.display = "none";
 
     toPhoto = 0;
-    var objPhotoBody = document.getElementById("imgChange");
-    objPhotoBody.style.background = "url(" + photo[toPhoto] + ") " + photoZoom;
-    objPhotoBody.style.backgroundSize = photoZoom2;
+    var objPhotoBody = document.getElementById("photoBody");
+    objPhotoBody.src = photo[toPhoto];
 }
 
 function nextPhoto() {
-    var objPhotoBody = document.getElementById("imgChange");
+    var objPhotoBody = document.getElementById("photoBody");
     toPhoto++;
-    objPhotoBody.style.background = "url(" + photo[toPhoto] + ") " + photoZoom;
-    objPhotoBody.style.backgroundSize = photoZoom2;
+    objPhotoBody.src = photo[toPhoto];
     if (toPhoto == photo.length) {
         toPhoto = 0;
-        objPhotoBody.style.background = "url(" + photo[toPhoto] + ") " + photoZoom;
-        objPhotoBody.style.backgroundSize = photoZoom2;
+        objPhotoBody.src = photo[toPhoto];
     }
 }
 
 function prevPhoto() {
-    var objPhotoBody = document.getElementById("imgChange");
+    var objPhotoBody = document.getElementById("photoBody");
     if (toPhoto == 0) {
         toPhoto = photo.length;
-        objPhotoBody.style.background = "url(" + photo[toPhoto] + ") " + photoZoom;
-        objPhotoBody.style.backgroundSize = photoZoom2;
+        objPhotoBody.src = photo[toPhoto];
     }
     toPhoto--;
-    objPhotoBody.style.background = "url(" + photo[toPhoto] + ") " + photoZoom;
-    objPhotoBody.style.backgroundSize = photoZoom2;
+    objPhotoBody.src = photo[toPhoto];
 }
