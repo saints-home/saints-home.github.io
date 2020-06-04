e58495b9e1b85299b3d1bfc5087d04ad260b7343
Ivan Chuvakhin <Chuvakhin@rospartner.ru>
2020-06-04 20:25:11 +0300

    asdsad

--- js/boot.js
+++ js/boot.js
@@ -43,21 +43,21 @@ function boot() {
     document.querySelector('body').insertAdjacentHTML('afterend', footerCode);
     $('#topmenu').hover(function() {
         var set = document.getElementById("submenu");
-        set.style.background = "#333333bd"
+        set.style.background = "#333333bd";
         set.style.visibility = "visible";
         set.style.opacity = "1";
-        set.style.transition = "0.25s"
+        set.style.transition = "visibility 0s, opacity 0.25s";
         var set1 = document.getElementById("topmenu");
         set1.style.background = "#0050ff";
-        set1.style.transition = "0.25s"
+        set1.style.transition = "0.25s";
     }, function() {
         var set = document.getElementById("submenu");
         set.style.visibility = "hidden";
         set.style.opacity = "0";
-        set.style.transition = "0s"
+        set.style.transition = "visibility 0s, opacity 0s";
         var set1 = document.getElementById("topmenu");
         set1.style.background = "";
-        set1.style.transition = "0.25s"
+        set1.style.transition = "0s";
       });
       document.querySelector('body').style.display = "block";
    }
